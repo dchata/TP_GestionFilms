c736c58a06a913057492f3c3a2749b32d23f64fd
dchata <chatagnierdylan@gmail.com>
2018-03-27 10:03:38 +0200

    BDD OK

--- DEV/GestionFilms.DB/Film.sql
+++ DEV/GestionFilms.DB/Film.sql
@@ -1,4 +1,14 @@
 ﻿CREATE TABLE [dbo].[Film]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id]			INT IDENTITY (1, 1) NOT NULL,
+	[IdGenre]		INT					NOT NULL,
+	[Name]			NVARCHAR (250)		NOT NULL,
+	[Duration]		TIME				NULL,
+	[ReleaseDate]	DATE				NULL,
+	[Score]			FLOAT				NULL,
+	[File]			NVARCHAR (250)		NOT NULL,
+	[Tag]			NVARCHAR (250)		NULL,
+	[Watched]		BIT					NULL,
+	CONSTRAINT [PK_Film] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_Film_Genre] FOREIGN KEY ([IdGenre]) REFERENCES [dbo].[Genre] ([Id])
 )
